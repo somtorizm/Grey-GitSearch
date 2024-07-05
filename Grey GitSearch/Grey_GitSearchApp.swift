e0de6c1221bf363c95b2a0268c075a1de56befc1
“somtorizm” <42660335+somtorizm@users.noreply.github.com>
2024-07-05 20:19:21 +0100

    Initial Commit

---
+++ Grey GitSearch/Grey_GitSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grey_GitSearchApp.swift
+//  Grey GitSearch
+//
+//  Created by Victor Somto on 05/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Grey_GitSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
